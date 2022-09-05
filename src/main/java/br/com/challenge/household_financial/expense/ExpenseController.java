4db27079edf6006440485620c3c85eb8a70a8b20
Daniel Hasegawa <dan_hasegawa@hotmail.com>
2022-09-05 19:25:12 -0300

    changing Expense to be able to filter by description

--- src/main/java/br/com/challenge/household_financial/expense/ExpenseController.java
+++ src/main/java/br/com/challenge/household_financial/expense/ExpenseController.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -27,22 +29,6 @@ public class ExpenseController {
 
 	@Autowired
 	private ExpenseRepository expenseRepository;
-
-	@GetMapping
-	public ResponseEntity<List<ExpenseResponseDto>> callAll() {
-		List<ExpenseResponseDto> list = expenseRepository.findAll().stream().map(e -> new ExpenseResponseDto(e))
-				.collect(Collectors.toList());
-		return ResponseEntity.ok(list);
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<ExpenseResponseDto> detail(@PathVariable Long id) {
-		Optional<Expense> expenseOptional = expenseRepository.findById(id);
-		if (expenseOptional.isPresent()) {
-			return ResponseEntity.ok(new ExpenseResponseDto(expenseOptional.get()));
-		}
-		return ResponseEntity.notFound().build();
-	}
 
 	@PostMapping
 	@Transactional
@@ -58,6 +44,37 @@ public class ExpenseController {
 
 		return ResponseEntity.created(uri).build();
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ExpenseResponseDto>> expenseDetail(@RequestParam(required = false) String description) {
+		List<ExpenseResponseDto> list = (Strings.isBlank(description) ? expenseRepository.findAll()
+				: expenseRepository.findByDescription(description)).stream().map(i -> new ExpenseResponseDto(i))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ExpenseResponseDto> detail(@PathVariable Long id) {
+		Optional<Expense> expenseOptional = expenseRepository.findById(id);
+		if (expenseOptional.isPresent()) {
+			return ResponseEntity.ok(new ExpenseResponseDto(expenseOptional.get()));
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@GetMapping("/{year}/{month}")
+	public ResponseEntity<List<ExpenseResponseDto>> expenseYearAndMonth(@PathVariable Integer year,
+			@PathVariable Integer month) {
+		if (year == null || month == null) {
+			return ResponseEntity.badRequest().build();
+		} else if (year < 1 || month > 12) {
+			return ResponseEntity.badRequest().build();
+		} else {
+			List<ExpenseResponseDto> list = expenseRepository.findByYearAndMonth(year, month).stream()
+					.map(e -> new ExpenseResponseDto(e)).collect(Collectors.toList());
+			return ResponseEntity.ok(list);
+		}
 	}
 
 	@PutMapping("/{id}")
