1434cbbc7e0aa27030b01a02907a3dcbd06fdb2b
Daniel Hasegawa <dan_hasegawa@hotmail.com>
2022-09-18 00:28:19 -0300

    refactoring incomeRepository

--- src/main/java/br/com/challenge/household_financial/income/IncomeRepository.java
+++ src/main/java/br/com/challenge/household_financial/income/IncomeRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface IncomeRepository extends JpaRepository<Income, Long> {
 
 	@Query("select count(c) from Income c where lower(c.description) = lower(:description) and month(c.date) = :month and year(c.date) = :year")
-	Long incomeByDescription(String description, int month, int year);
+	Long incomeByDescription(String description,  int month, int year);
 
 	List<Income> findByDescription(String description);
 	
